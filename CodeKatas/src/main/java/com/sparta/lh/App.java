f9246f7cc794e10b7dec22e6acfab9bc1b083f60
l-hepper <liamhepper1@gmail.com>
2024-06-18 16:31:51 +0100

    add CodeKatas project with IntegerArrayToInt exercise and basic unit test

---
+++ CodeKatas/src/main/java/com/sparta/lh/App.java
@@ -0,0 +1,10 @@
+package com.sparta.lh;
+
+public class App {
+    public static void main(String[] args) {
+        int[] numbers = {1, 0, 2, 4};
+        System.out.println(ArrayOfIntegersToInt.method(numbers));
+    }
+
+
+}
---
+++ CodeKatas/src/main/java/com/sparta/lh/ArrayOfIntegersToInt.java
@@ -0,0 +1,24 @@
+package com.sparta.lh;
+
+//        Given an array of integers, convert them to a single int made up of the digits
+//
+//        for example the array:
+//
+//        int[] numbers = {1, 0, 2, 4}
+//
+//        should return the int 1024.
+//
+//        Use TDD and ask if you need any requirements clarified
+
+public class ArrayOfIntegersToInt {
+
+    public static int method(int[] numbers) {
+
+        StringBuilder newString = new StringBuilder();
+        for (int number : numbers) {
+            newString.append(number);
+        }
+
+        return Integer.parseInt(String.valueOf(newString));
+    }
+}
