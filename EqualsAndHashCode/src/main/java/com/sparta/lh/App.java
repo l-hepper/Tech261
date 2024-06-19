0d14d2de5eca18d3bb21cd5b0b8230241f3621b8
l-hepper <liamhepper1@gmail.com>
2024-06-19 15:34:51 +0100

    add override methods for hashCode() and equals() in Sparta class

--- EqualsAndHashCode/src/main/java/com/sparta/lh/App.java
+++ EqualsAndHashCode/src/main/java/com/sparta/lh/App.java
@@ -2,6 +2,10 @@ package com.sparta.lh;
 
 public class App {
     public static void main(String[] args) {
+        Spartan spartan1 = new Spartan("Manish", "Gadhvi");
+        Spartan spartan2 = new Spartan("Manish", "Gadhvi");
+        System.out.println(spartan1.equals(spartan2));
         
+
     }
 }
--- EqualsAndHashCode/src/main/java/com/sparta/lh/Spartan.java
+++ EqualsAndHashCode/src/main/java/com/sparta/lh/Spartan.java
@@ -1,5 +1,7 @@
 package com.sparta.lh;
 
+import java.util.Objects;
+
 public class Spartan {
     private String firstName;
     private String lastName;
@@ -24,4 +26,35 @@ public class Spartan {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Spartan spartan = (Spartan) o;
+//        return Objects.equals(firstName, spartan.firstName) && Objects.equals(lastName, spartan.lastName);
+//    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;   // if this instance is the same memory address as o then return true they must be the same
+        }
+
+        // if this is a spartan object
+        if (object instanceof Spartan spartan) {
+
+            // if they are the same, then do they have the same content?
+            if (this.getFirstName().equals(spartan.getFirstName()) && (this.getLastName().equals(spartan.getLastName()))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(firstName, lastName);
+//    }
 }
