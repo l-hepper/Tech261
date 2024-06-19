61b9e71df53c23d2ca790c1b746f4f51ec50c307
l-hepper <liamhepper1@gmail.com>
2024-06-19 12:29:39 +0100

    mini lab project completed

--- AbstractClassesMiniLab/src/main/java/com/sparta/lh/App.java
+++ AbstractClassesMiniLab/src/main/java/com/sparta/lh/App.java
@@ -1,7 +1,53 @@
 package com.sparta.lh;
 
+//Create an abstract class called Trainee with the following fields:
+//
+//name
+//        age
+//email
+//        phone
+//Create a subclass called JavaTrainee that extends Trainee and has the following fields:
+//
+//experience
+//        certifications
+//projects
+//        github
+//Create a subclass called DevOpsTrainee that extends Trainee and has the following fields:
+//
+//experience
+//        certifications
+//projects
+//Create an interface called Trainable with the following methods:
+//
+//train()
+//study()
+//Make the JavaTrainee and DevOpsTrainee classes implement the Trainable interface.
+//
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+
+        Trainee liam = new JavaTrainee(
+                "Liam",
+                32,
+                "liam@mail.com",
+                "12345",
+                "Lots of experience",
+                "No certs",
+                "Samurise",
+                "liam@github");
+
+        Trainee bob = new DevOpsTrainee(
+                "Bob",
+                25,
+                "bob@mail.com",
+                "54321",
+                "no experience",
+                "CSSIP",
+                "251 To-Do Apps");
+
+        System.out.println(liam);
+        System.out.println(bob);
     }
 }
