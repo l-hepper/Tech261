84f4e932a52cca00f46f7d661d9633f22860aabe
l-hepper <liamhepper1@gmail.com>
2024-06-20 14:48:28 +0100

    add more efficient isPalindrome method using StringBuilder reverse method

--- Strings/src/main/java/com/sparta/lh/PalindromeChecker.java
+++ Strings/src/main/java/com/sparta/lh/PalindromeChecker.java
@@ -1,6 +1,25 @@
 package com.sparta.lh;
 
 public class PalindromeChecker {
+
+    // the leetcode way
+//    public static boolean isPalindrome(String string) {
+//        if (string == null || string.length() < 3) {
+//            return false;
+//        }
+//
+//        string = string.toLowerCase();
+//
+//        for (int i = 0, j = string.length() - 1; i < string.length() / 2; i++, j--) {
+//            if (string.charAt(i) != string.charAt(j)) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+
+    // the more efficient way using StringBuilder
     public static boolean isPalindrome(String string) {
         if (string == null || string.length() < 3) {
             return false;
@@ -8,12 +27,6 @@ public class PalindromeChecker {
 
         string = string.toLowerCase();
 
-        for (int i = 0, j = string.length() - 1; i < string.length() / 2; i++, j--) {
-            if (string.charAt(i) != string.charAt(j)) {
-                return false;
-            }
-        }
-
-        return true;
+        return new StringBuilder(string).reverse().toString().equals(string);
     }
 }
