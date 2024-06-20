42d6d33d8bf26995db3fd3ec093effe53be7f15f
l-hepper <liamhepper1@gmail.com>
2024-06-20 16:51:50 +0100

    added getLongestPalindromes method with unit tests

--- Strings/src/main/java/com/sparta/lh/App.java
+++ Strings/src/main/java/com/sparta/lh/App.java
@@ -4,7 +4,6 @@ import java.util.Objects;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(PalindromeChecker.isPalindrome(" racecar "));
 
     }
 }
--- Strings/src/main/java/com/sparta/lh/StringExample.java
+++ Strings/src/main/java/com/sparta/lh/StringExample.java
@@ -2,13 +2,6 @@ package com.sparta.lh;
 
 public class StringExample {
     public static void main(String[] args) {
-        String string1 = "Hello";
-        string1.toUpperCase();
-        System.out.println(string1);
 
-        System.out.println(string1.toCharArray());
-
-        String name = "Liam";
-        System.out.println("Hello " + name);
     }
 }
