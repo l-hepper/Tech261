471bd87129a1c7c47c7f67a5838ca05a88145949
l-hepper <liamhepper1@gmail.com>
2024-06-27 11:29:45 +0100

    add design patterns, generics, and logging project

--- CodeKata2IdenticalNeighbours/src/main/java/com/sparta/lh/App.java
+++ CodeKata2IdenticalNeighbours/src/main/java/com/sparta/lh/App.java
@@ -1,18 +1,55 @@
 package com.sparta.lh;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.*;
+
 public class App {
 
+    private static final Logger logger = Logger.getLogger(App.class.getName());
+
+    public static void main(String[] args) {
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+
+        // there is simple formatter and XML formatter
+        consoleHandler.setFormatter(new SimpleFormatter());
+        logger.addHandler(consoleHandler);
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false); // this means the logger won't do anything
+
+        try {
+            FileHandler fileHandler = new FileHandler("src/main/resources/logfile.log");
+            fileHandler.setLevel(Level.ALL);
+            fileHandler.setFormatter(new XMLFormatter());
+            logger.addHandler(fileHandler);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        int[] array1 = {1, 2, 3, 4, 5};
+        hasThreeIdenticalNeighbours(array1);
+
+        int[] array2 = {1, 1, 1, 4, 5};
+        hasThreeIdenticalNeighbours(array2);
+    }
+
     public static boolean hasThreeIdenticalNeighbours(int[] numbers) {
+        logger.info("called with argument " + Arrays.toString(numbers));
+
         if (numbers.length < 3) {
+            logger.info("array.length < 3 - return FALSE");
             return false;
         }
 
         for (int i = 2; i < numbers.length; i++) {
             if (numbers[i] == numbers[i-1] && numbers[i] == numbers[i-2]) {
+                logger.fine("neighbours found - return TRUE");
                 return true;
             }
         }
 
+        logger.info("no neighbours found - return FALSE");
         return false;
     }
 }
--- DesignPatterns/src/main/java/com/sparta/lh/App.java
+++ DesignPatterns/src/main/java/com/sparta/lh/App.java
@@ -1,7 +1,21 @@
 package com.sparta.lh;
 
+import com.sparta.lh.decorator.Italian;
+import com.sparta.lh.decorator.sides.Chips;
+import com.sparta.lh.decorator.sides.Salad;
+import com.sparta.lh.singleton.SingletonExample;
+
 public class App {
     public static void main(String[] args) {
+        Italian italian = new Italian();
+        System.out.println(italian.getDescription() + " Cost:" + italian.getCost());
+
+        Salad salad = new Salad(italian);
+
+        System.out.println(salad.getDescription() + " Cost:" + salad.getCost());
+
+        Chips chips = new Chips(italian);
+        System.out.println(chips.getDescription() + "Cost: "+ chips.getCost());
 
     }
 }
